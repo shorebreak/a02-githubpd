1ae3de0bc78dd5db23a77b02d7ce137e0f2a6dc7
shorebreak <11183661+shorebreak@users.noreply.github.com>
2023-05-17 10:48:48 -0700

    added color changes to background

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    scene.setBackgroundColor(10)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    scene.setBackgroundColor(5)
+})
+scene.setBackgroundColor(3)
